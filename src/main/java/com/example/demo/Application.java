c0af743865149057b4b039f179baba35439a5bad
Ashok IT <roy.anisha2022@gmail.com>
2021-11-19 00:45:45 +0530

    added feature 301

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,11 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+      Map<String,Object> cache = new HashMap<String,Object>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("running");
 
                  int a=10;
 	}
+
+   public void loadDataToCache(){
+      //logic
+}
 
 }
